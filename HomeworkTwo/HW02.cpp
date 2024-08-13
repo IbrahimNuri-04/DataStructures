f5dd19c9a62e2b05113d08636f7f5aadcc35863d
𝐈𝐛𝐫𝐚𝐡𝐢𝐦 𝐍𝐮𝐫𝐢 <sci_223745@omu.edu.ly>
2024-08-14 00:58:19 +0200

    Add files via upload

---
+++ HomeworkTwo/HW02.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main () {
+    string name = "Ibrahim Nuri";
+    string ID = "223745";
+    
+    cout<<"Name : "<<name<<endl;
+    cout<<"ID : "<< ID<<endl;
+
+    return 0;
+}
