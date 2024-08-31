059a280c8962e217832615144f068616df0e13e2
𝐈𝐛𝐫𝐚𝐡𝐢𝐦 𝐍𝐮𝐫𝐢 <sci_223745@omu.edu.ly>
2024-08-31 12:17:06 +0200

    Create HomeworkFive

---
+++ HomeworkFive/HW05_Q1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Student {
+    int ID;
+    string Name;
+    int Age;
+    Student* ptr; 
+};
+
+int main() {
+    
+    Student student1 = {1001, "John Doe", 20};
+    Student student2 = {1002, "Jane Smith", 21};
+    Student student3 = {1003, "Bob Johnson", 19};
+    
+   student1.ptr = &student2;
+   student2.ptr = &student3;
+
+   Student* current = &student1;
+   int studentNum = 1;
+
+   while (current != nullptr) {
+        cout << "Student : " << studentNum<<endl;        
+        cout << "ID: " << current->ID<<endl;
+        cout << "Name: " << current->Name <<endl;
+        cout << "Age: " << current->Age <<endl<<endl;
+        
+        current = current->ptr; 
+        studentNum++;
+    }
+
+   return 0;
+}
